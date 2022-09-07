60c99ce3e222d19785bd1650bf55c44b5d221d1d
Ayush Siloiya <85118674+ayushsiloiya619@users.noreply.github.com>
2022-09-07 21:47:39 +0530

    update

--- Basics/hi.py
+++ Basics/hi.py
@@ -1,1 +1,3 @@
 print("Hey"+" "+input("What is your name ?")+" "+ "how are you?")
+<br>
+<img src="https://github.com/ayushsiloiya619/Python-Programs/blob/main/Problems%20pics/hi.png" class="img-fluid" alt="Responsive image">
