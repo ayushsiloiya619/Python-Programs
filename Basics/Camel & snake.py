5e1909b92b61b425d1453c405f2000e7c4ff39a0
Ayush Siloiya <85118674+ayushsiloiya619@users.noreply.github.com>
2022-09-09 00:54:50 +0530

    camel_snake

---
+++ Basics/Camel & snake.py
@@ -0,0 +1,8 @@
+MyName=input(("What is your name ?"))
+print(MyName)
+myName=input(("What is your name ?"))
+print(myName)
+myname=input(("What is your name ?"))
+print(myname)
+# The output is:
+# What is your name ?
